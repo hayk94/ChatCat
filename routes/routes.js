560a6fa9580bccde0ec361cf1fbb5c590ce35cd6
hayk94 <hayk94@hotmail.com>
2016-08-02 15:29:13 +0400

    Created the sessions
    added the cookie parser and express session

--- routes/routes.js
+++ routes/routes.js
@@ -8,5 +8,14 @@ module.exports = function (express, app) {
   router.get('/chatrooms', function (req,res,next) {
     res.render('chatrooms', {tite:'Chatrooms'});
   });
+
+  router.get('/setcolor', function (req,res,next) {
+    req.session.favColor = 'Red';
+    res.send('setting favourite color!');
+  });
+  router.get('/getcolor', function (req,res,next) {
+    res.send('Favourite Color ' + (req.session.favColor===undefined?"Not Found":req.session.favColor));
+  });
+
   app.use('/',router);
 };
