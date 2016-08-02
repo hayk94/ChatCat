d0b45742a0f7caded4c9ade2a1facecb9ca88d67
hayk94 <hayk94@hotmail.com>
2016-08-02 14:54:24 +0400

    Initialized the express and hogan-express
    Hogan express for the view templating

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+app.set('views',path.join(__dirname, 'views'));
+app.engine('html',require('hogan-express'));
+app.set('view engine','html');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.route('/').get(function (req,res,next) {
-  res.send('<h1>Hello World!</h1>');
+  // res.send('<h1>Hello World!</h1>');
+  res.render('index', {title:'Welcome to ChatCAT'});
 });
 
 app.listen(3000, function () {
