c6d4f82e6bb9d450e8e025ba06359a2281a6e97a
hayk94 <hayk94@hotmail.com>
2016-08-03 17:05:53 +0400

    Sending the message updating the message fields

--- socket/socket.js
+++ socket/socket.js
@@ -8,4 +8,15 @@ module.exports = function (io,rooms) {
       socket.emit('roomupdate',JSON.stringify(rooms));
     });
   });
+  var messages = io.of('/messages').on("connection", function(socket) {
+    console.log('connected to the chatroom!');
+    socket.on('joinroom', function(data) {
+      socket.username = data.user;
+      socket.userPic = data.userPic;
+      socket.join(data.room);
+    });
+    socket.on('newMessage', function(data) {
+      socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+    });
+  });
 };
