b81f1f2d49a9789bd1bb48fd1a80c592c57dc2b0
hayk94 <hayk94@hotmail.com>
2016-08-03 17:52:54 +0400

    Updating active users in chatroom
    NOT WORKING!!!

--- socket/socket.js
+++ socket/socket.js
@@ -14,9 +14,26 @@ module.exports = function (io,rooms) {
       socket.username = data.user;
       socket.userPic = data.userPic;
       socket.join(data.room);
+      updateUsersList(data.room,true);
     });
     socket.on('newMessage', function(data) {
       socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+    });
+    function updateUsersList(room, updateALL) {
+      // var getUsers = io.of('/messages').clients(room); OLD VERSION
+      var getUsers = io.of('/messages').in(room).clients;
+      var userlist = [];
+      for (var i in getUsers) {
+        userlist.push({user:getUsers[i].username, userPic:getUsers[i].userPic});
+      }
+      socket.to(room).emit('updateUsersList',JSON.stringify(userlist));
+      if (updateALL) {
+        socket.broadcast.to(room).emit('updateUsersList', JSON.stringify(userlist));
+      }
+    }
+    socket.on('updateList', function(data) {
+      updateUsersList(data.room);
+      console.log('updateList triggered in socket.js');
     });
   });
 };
